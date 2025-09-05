967a21de0f5a22f7a31b9ebc27cf66e27511f316
RviP9973 <rp031776@gmail.com>
2025-09-05 10:42:19 +0530

    tailwind fix

--- farmer_support_system/react-frontend/src/App.jsx
+++ farmer_support_system/react-frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Layout } from "./components";
-import {
-  ChatPage,
-  DiseasePredictionPage,
-  DashboardPage,
-  NotFoundPage,
-} from "./pages";
-import { ChatProvider, DocumentProvider } from "./context";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components';
+import { ChatPage, DiseasePredictionPage, DashboardPage, NotFoundPage } from './pages';
+import { ChatProvider, DocumentProvider } from './context';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<ChatPage />} />
-        <Route path="disease-prediction" element={<DiseasePredictionPage />} />
-        <Route path="dashboard" element={<DashboardPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <ChatProvider>
+      <DocumentProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<ChatPage />} />
+            <Route path="disease-prediction" element={<DiseasePredictionPage />} />
+            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </DocumentProvider>
+    </ChatProvider>
   );
 }
 
